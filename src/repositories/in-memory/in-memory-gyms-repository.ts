4d049184471d95cd176176b5384a8f6b9c127615
Kassio Matos <kassio_29@hotmail.com>
2025-11-15 11:24:31 -0300

    fix: adjust expected param to latitute and longitude

--- src/repositories/in-memory/in-memory-gyms-repository.ts
+++ src/repositories/in-memory/in-memory-gyms-repository.ts
@@ -42,8 +42,8 @@ export class InMemoryGymsRepository implements GymsRepository {
       title: data.title,
       description: data.description ?? null,
       phone: data.phone ?? null,
-      latitude: new Prisma.Decimal(data.latitude.toString()),
-      longitude: new Prisma.Decimal(data.longitude.toString()),
+      latitude: Prisma.Decimal(data.latitude.toString()),
+      longitude: Prisma.Decimal(data.longitude.toString()),
       created_at: new Date(),
     }
 
