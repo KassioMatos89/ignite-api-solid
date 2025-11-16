88bf56f4b058797ff05afd468b51eb6f5fc9ed57
Kassio Matos <kassio_29@hotmail.com>
2025-11-15 23:05:29 -0300

    feat: adjust imports in in-memory-gyms-repository

--- src/repositories/in-memory/in-memory-gyms-repository.ts
+++ src/repositories/in-memory/in-memory-gyms-repository.ts
@@ -1,7 +1,7 @@
-import { Gym, Prisma } from '@prisma/client'
 import { FindManyNearbyParams, GymsRepository } from '../gyms-repository'
-import { randomUUID } from 'node:crypto'
 import { getDistanceBetweenCoordinates } from '@/utils/get-distance-between-coordinates'
+import { Gym, Prisma } from '@prisma/client'
+import { randomUUID } from 'node:crypto'
 
 export class InMemoryGymsRepository implements GymsRepository {
   public items: Gym[] = []
@@ -42,8 +42,8 @@ export class InMemoryGymsRepository implements GymsRepository {
       title: data.title,
       description: data.description ?? null,
       phone: data.phone ?? null,
-      latitude: Prisma.Decimal(data.latitude.toString()),
-      longitude: Prisma.Decimal(data.longitude.toString()),
+      latitude: new Prisma.Decimal(data.latitude.toString()),
+      longitude: new Prisma.Decimal(data.longitude.toString()),
       created_at: new Date(),
     }
 
